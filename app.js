595721aa9fe0a4239be853e969d21213d34f6a42
Matias Sanchez <tute.sanchez@gmail.com>
2020-06-14 19:43:12 -0300

    methods functions

--- app.js
+++ app.js
@@ -34,3 +34,21 @@ var exercise = new Vue({
 		},
 	},
 });
+
+var button = new Vue({
+	el: '#button-test',
+	data: {
+		counter: 0,
+		x: 0,
+		y: 0,
+	},
+	methods: {
+		increase: function (step) {
+			this.counter += step;
+		},
+		updateCoordinates(event) {
+			this.x = event.clientX;
+			this.y = event.clientY;
+		},
+	},
+});
